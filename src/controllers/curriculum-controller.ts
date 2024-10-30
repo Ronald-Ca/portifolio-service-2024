c17f75dcfae6561c794b4f47f562143ffa65bdd5
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-10-29 20:14:57 -0400

    criando rotina para curriculo

---
+++ src/controllers/curriculum-controller.ts
@@ -0,0 +1,56 @@
+import { responseError, responseSuccess } from "@utils/jsonResponse"
+import CurriculumService from "../services/curriculum-service"
+import { Request, Response } from 'express'
+import InternalError from "@utils/internalError"
+import fileUpload from "express-fileupload"
+import Upload, { CloudinaryUploadResult } from "../integrations/cloudnary"
+import { validId } from "../zod-validations/global/valid-id"
+
+export default class CurriculumController {
+    private _curriculumService = new CurriculumService()
+
+    async getCurriculum(_: Request, res: Response) {
+        try {
+            const response = await this._curriculumService.getCurriculum()
+
+            return res.status(200).json(responseSuccess('Success', response))
+        } catch (error) {
+            if (error instanceof InternalError) throw new InternalError(error.message)
+            throw error
+        }
+    }
+
+    async create(req: Request, res: Response) {
+        try {
+            const curriculum = req.files?.curriculum as fileUpload.UploadedFile
+            const upload = curriculum && await Upload(curriculum, 'curriculum') as CloudinaryUploadResult
+
+            const response = await this._curriculumService.create({ curriculum: upload?.secure_url })
+
+            return res.status(200).json(responseSuccess('Success', response))
+
+        } catch (error) {
+            if (error instanceof InternalError) throw new InternalError(error.message)
+            throw error
+        }
+    }
+
+    async update(req: Request, res: Response) {
+        try {
+            const { id } = validId.parse(req.params)
+
+            const existCurriculum = await this._curriculumService.getCurriculumById(id)
+            if (!existCurriculum) return res.status(404).json(responseError(['Curriculum not found']))
+
+            const curriculum = req.files?.curriculum as fileUpload.UploadedFile
+            const upload = curriculum && await Upload(curriculum, 'curriculum') as CloudinaryUploadResult
+
+            const response = await this._curriculumService.update(id, { curriculum: upload?.secure_url })
+
+            return res.status(200).json(responseSuccess('Curriculum updated', response))
+        } catch (error) {
+            if (error instanceof InternalError) throw new InternalError(error.message)
+            throw error
+        }
+    }
+}
---
+++ src/services/curriculum-service.ts
@@ -0,0 +1,29 @@
+import { Prisma } from '@prisma/client'
+import { PrismaService } from '@prisma/prisma-service'
+
+export default class CurriculumService {
+
+    async getCurriculum() {
+        const data = await PrismaService.curriculum.findFirst()
+
+        return data
+    }
+
+    async getCurriculumById(id: string) {
+        const data = await PrismaService.curriculum.findUnique({ where: { id } })
+
+        return data
+    }
+
+    async create(curriculum: Prisma.CurriculumCreateInput) {
+        const data = await PrismaService.curriculum.create({ data: curriculum })
+
+        return data
+    }
+
+    async update(id: string, curriculum: Prisma.CurriculumUpdateInput) {
+        const data = await PrismaService.curriculum.update({ where: { id }, data: curriculum })
+
+        return data
+    }
+}
